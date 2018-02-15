4c006cdb96898ec6f22167544efdbb1cc4bbe57b
unknown <C:\Users\Sergei\AppData\Roaming\The Bat!>
2018-02-14 22:47:48 -0800

    added angular material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
+import { MatListModule, MatExpansionModule } from '@angular/material'
 
 import { AppComponent } from './app.component'
 import { ElevatorsComponent } from './components/elevators/elevators.component'
 import { ElevatorComponent } from './components/elevator/elevator.component'
 import { BuildingService } from './services/building.service'
 
+/*
+NOTE: import angular material modules after BrowserModule, because order matters.
+*/
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,7 +19,9 @@ import { BuildingService } from './services/building.service'
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatListModule,
+    MatExpansionModule
   ],
   providers: [
     BuildingService
